73445bf8afa4a5dec744386c3cad8ab1e3dfcfd3
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-07-13 09:13:40 +0800

    Revert "Change CutoffDate:DateTime to CutoffID:string."
    This reverts commit 8866d665448b1f52cee04f2eb41d57001cf9f246.

--- Payroll.Timesheets.Domain/Timesheet.cs
+++ Payroll.Timesheets.Domain/Timesheet.cs
@@ -16,7 +16,14 @@ namespace Payroll.Timesheets.Domain
         public string EEId { get; set; }
         public virtual EmployeeView EE { get; set; }
 
-        public string CutoffId { get; set; }
+        public DateTime CutoffDate { get; set; }
+
+        //public string PayRegisterId { get; set; }
+        //[ForeignKey("PayRegisterId")]
+        //public virtual PayRegister PayRegister { get; set; }
+
+        //[NotMapped]
+        //public List<AdjustmentBilling> Billings { get; set; }
 
         public string? PayrollCode { get; set; }
         public string? BankCategory { get; set; }
@@ -42,10 +49,7 @@ namespace Payroll.Timesheets.Domain
         [JsonProperty("allowance")]
         public double Allowance { get; set; }
 
-        [NotMapped]
         [JsonProperty("pcv")]
-        public string[][] PCV { get; set; }
-
         public string RawPCV { get; set; }
 
         [JsonProperty("is_confirmed")]
