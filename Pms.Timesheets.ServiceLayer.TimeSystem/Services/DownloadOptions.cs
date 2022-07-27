4ad966c564961c2b9eb1c83ca7d974ca3d0acbd2
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-07-27 15:10:43 +0800

    Added a Download Options Enum.

---
+++ Pms.Timesheets.ServiceLayer.TimeSystem/Services/DownloadOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Timesheets.ServiceLayer.TimeSystem.Services
+{
+    public static class Enums
+    {
+        public enum DownloadOptions
+        {
+            All,
+            UnconfirmedOnly
+        }
+    }
+}
