cda61902e6eeb25a70aba3d63999672766769fbc
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-07-29 16:23:44 +0800

    Added two methods for extracting a list of Payroll Codes and Bank Categories.

--- Pms.Timesheets.ServiceLayer.EfCore/QueryObjects/GroupByExt.cs
+++ Pms.Timesheets.ServiceLayer.EfCore/QueryObjects/GroupByExt.cs
@@ -14,6 +14,23 @@ namespace Pms.Timesheets.ServiceLayer.EfCore.QueryObjects
                 .GroupBy(ts => ts.Page, ts => ts.Page)
                 .Select((page, i) => page.First())
                 .ToList();
+ 
 
+
+        public static List<string> ExtractBankCategories(this IEnumerable<Timesheet> timesheets, string payrollCode) =>
+            timesheets.ToList().Where(ts => ts.BankCategory != "")
+                .Where(ts => ts.PayrollCode == payrollCode)
+                .GroupBy(ts => ts.BankCategory)
+                .Select(ts => ts.First())
+                .OrderBy(ts => ts.BankCategory)
+                .Select(ts => ts.BankCategory).ToList();
+
+
+        public static List<string> ExtractPayrollCodes(this IEnumerable<Timesheet> timesheets) =>
+            timesheets.ToList().Where(ts => ts.PayrollCode != "")
+                .GroupBy(ts => ts.PayrollCode)
+                .Select(ts => ts.First())
+                .OrderBy(ts => ts.PayrollCode)
+                .Select(ts => ts.PayrollCode).ToList();
     }
 }
