442f14303112257405cc7b17c8765af44f10a1e5
Sean Ivan Fernandez_SW Programmer <seanivanf@gmail.com>
2022-08-05 23:52:53 +0800

    Created an interface for Timesheet services.

---
+++ Pms.Timesheets.ServiceLayer.EfCore/ITimesheetPageProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Timesheets.ServiceLayer.EfCore
+{
+    public interface ITimesheetPageProvider
+    {
+        public int GetLastPage(string cutoffId, string payrollCode);
+
+        public List<int> GetPageWithUnconfirmedTS(string cutoffId, string payrollCode);
+
+        public List<int> GetPages(string cutoffId, string payrollCode);
+        
+        public List<int> GetMissingPages(string cutoffId, string payrollCode);
+    }
+}
---
+++ Pms.Timesheets.ServiceLayer.EfCore/ITimesheetProvider.cs
@@ -0,0 +1,24 @@
+﻿using Pms.Timesheets.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pms.Timesheets.ServiceLayer.EfCore
+{
+    public interface ITimesheetProvider
+    {
+        IEnumerable<Timesheet> GetTimesheets();
+
+        IEnumerable<Timesheet> GetTimesheetsByCutoffId(string cutoffId);
+
+        IEnumerable<Timesheet> GetTimesheetsByCutoffId(string cutoffId, string payrollCode);
+
+        IEnumerable<Timesheet> GetTimesheetsByCutoffId(string cutoffId, string payrollCode, string bankCategory);
+
+        IEnumerable<Timesheet> GetTimesheetNoEETimesheet(string cutoffId);
+
+        List<string> ListTimesheetBankCategory(string payrollCode);
+    }
+}
